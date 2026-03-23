18c4466c1d1a99c316e357f2f294b156aa79e237
Sergi Cortés <sergi_cortes@hotmail.com>
2026-03-23 09:01:44 +0100

    fix: terminal close

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,11 +3,25 @@ vim.pack.add({
 	{ src = "https://github.com/waiting-for-dev/toggleterm.nvim", branch = "waiting-for-dev/wop" },
 })
 
-require("toggleterm").setup()
+require("toggleterm").setup({
+	size = 20,
+	open_mapping = [[<c-\>]],
+	hide_numbers = true,
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 2,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "rounded",
+	},
+})
 
-vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
-vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle Floating Terminal" })
+vim.keymap.set({ "n", "t" }, "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
 vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle Horizontal Terminal" })
-vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Toggle Vertical Terminal" })
 vim.keymap.set("n", "<leader>tl", "<cmd>ToggleTermSendCurrentLine <T_ID><cr>", { desc = "Toggle Send Current Line" })
 vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTermSendVisualLines <T_ID><cr>", { desc = "Toggle Send Visual Lines" })
