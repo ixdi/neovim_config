ecfadbda141a38b191106b7c7e6b27bfcd679f29
Sergi Cortés <sergi_cortes@hotmail.com>
2026-06-01 13:21:33 +0200

    refactor: mini files

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -18,16 +18,16 @@ require("mini.files").setup(
 		-- Module mappings created only inside explorer.
 		-- Use `''` (empty string) to not create one.
 		mappings = {
-			close = "q",
-			go_in = "l",
-			go_in_plus = "L",
-			go_out = "h",
-			go_out_plus = "H",
+			close = "<esc>",
+			go_in = "L",
+			go_in_plus = "l",
+			go_out = "H",
+			go_out_plus = "h",
 			mark_goto = "'",
 			mark_set = "m",
 			reset = "<BS>",
 			reveal_cwd = "@",
-			show_help = "g?",
+			show_help = "?",
 			synchronize = "=",
 			trim_left = "<",
 			trim_right = ">",
@@ -48,7 +48,7 @@ require("mini.files").setup(
 			-- Maximum number of windows to show side by side
 			max_number = math.huge,
 			-- Whether to show preview of file/directory under cursor
-			preview = false,
+			preview = true,
 			-- Width of focused window
 			width_focus = 50,
 			-- Width of non-focused window
@@ -59,4 +59,62 @@ require("mini.files").setup(
 	}
 )
 
-vim.api.nvim_set_keymap("n", "ee", "<cmd>lua MiniFiles.open()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"ee",
+	"<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Helper function to map keys for splitting
+local map_split = function(buf_id, lhs, direction)
+	local rhs = function()
+		-- Make new window and set it as target
+		local cur_target = MiniFiles.get_explorer_state().target_window
+		local new_target = vim.api.nvim_win_call(cur_target, function()
+			vim.cmd(direction .. " split")
+			return vim.api.nvim_get_current_win()
+		end)
+
+		MiniFiles.set_target_window(new_target)
+
+		-- This intentionally doesn't act on file under cursor in favor of
+		-- explicit "go in" action (`l` / `L`). To immediately open file,
+		-- add appropriate `MiniFiles.go_in()` call instead of this comment.
+		MiniFiles.go_in({ close_on_file = true })
+	end
+
+	-- Adding `desc` will result into `show_help` entries
+	local desc = "Split " .. direction
+	vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+-- Show dotfiles in explorer
+local show_dotfiles = true
+
+local filter_show = function(fs_entry)
+	return true
+end
+
+local filter_hide = function(fs_entry)
+	return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+	show_dotfiles = not show_dotfiles
+	local new_filter = show_dotfiles and filter_show or filter_hide
+	MiniFiles.refresh({ content = { filter = new_filter } })
+end
+
+-- Set initial filter
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		-- Tweak keys to your liking
+		map_split(buf_id, "-", "belowright horizontal")
+		map_split(buf_id, "+", "belowright vertical")
+		map_split(buf_id, "t", "tab")
+		vim.keymap.set("n", "<S-i>", toggle_dotfiles, { buffer = buf_id })
+	end,
+})
